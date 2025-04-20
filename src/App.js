cd59f0702a9bee5fb7984bf5d81bf2c18c96ecab
Isaak Morales <ice.isaak97@gmail.com>
2025-04-20 17:36:20 -0400

    made contact and reinstalled dependencies

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router";
 import React from "react";
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
 import Menu from "./components/Menu";
 import Home from "./components/Home";
-
-
 
 function App() {
   return (
     <div className="App">
-      <Router>
-          <Routes> 
-            <Route path="/" element={<Home/>} />
-            <Route path="/menu" element={<Menu/>} />
-          </Routes>
-          </Router>
+        <Routes>
+          <Route path="/" element={<Home/> } />
+          <Route path="/menu" element={<Menu /> } />
+        </Routes>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Home() {
 
-<h1>Home</h1>
+return <h1>Home</h1>
 
 }
