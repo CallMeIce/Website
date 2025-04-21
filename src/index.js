2dd06f0c70d0f9d9727cc57df5840e9f92aa3b0c
Isaak Morales <ice.isaak97@gmail.com>
2025-04-21 12:07:35 -0400

    adding navbar

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <Header />
     <App />
     </BrowserRouter>
   </React.StrictMode>
