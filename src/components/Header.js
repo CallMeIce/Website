1bece76d3857ea73716479173d47ca05b0f45eb1
Isaak Morales <ice.isaak97@gmail.com>
2025-04-21 12:39:47 -0400

    finished navbar layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,35 @@ import React from "react";
 
 export default function Header() {
 
+    const styles = {
+        imageStyle: {
+            width: '25%',
+            borderRadius: 30,
+          }      
+    }
+
 return (
 
-<nav class="navbar navbar-light bg-light">
+<nav class="navbar sticky-top navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">
-      
-    </a>
+    <img style={styles.imageStyle} src={require('/users/isaak/Desktop/Home-Projects/Website/src/images/Logo.jpg')} class="img-thumbnail" alt="Pub Logo"></img>
+        Pub
+        </a>
+        <ul class="nav justify-content-end">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">About Us</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Menu</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Meet the Team</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Contact Us</a>
+  </li>
+</ul>
   </div>
 </nav>
 
