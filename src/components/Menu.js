bbb176c3807bf2d8fe10e86c559dfad3708fb8a9
Isaak Morales <ice.isaak97@gmail.com>
2025-04-29 18:02:18 -0400

    made columns for the menu just need to add pic/carousel

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,48 +4,52 @@ import "./style.css";
 export default function Menu() {
   return (
     <div className="menuFont">
+      <br></br>
       <h1>Menu</h1>
+      <h2><br></br>Appetizers</h2>
+      <br></br>
+      <div class="row">
+    <div class="col-4">col-8</div>
+    <div class="col-8">
+      <ul>
+      <li>$6 - Mozzarella Sticks ~or~ French Fries ~or~ Onion Rings</li>
+      <li>$8 - Potato Skins – Crispy potato halves loaded with cheese, bacon, and sour cream.</li>
+      <li>$10 - Chicken Wings (BBQ, Buffalo, Mild, Teriyaki)</li>
+      <li>$12 - Irish Nachos – Sliced potatoes topped with melted cheese, jalapeños, and sour cream.</li>
+      <li>$22 - The Four Leaf Clover (Mix of the 4 appetizers)</li>
+        </ul>
+        </div>
+  </div>
       <div>
-        $6	Mozzarella Sticks ~or~ French Fries ~or~ Onion Rings
-$8	Potato Skins – Crispy potato halves loaded with cheese, bacon, and sour cream. 
-$10	Chicken Wings (BBQ, Buffalo, Mild, Teriyaki)
-$12	Irish Nachos – Sliced potatoes topped with melted cheese, 	jalapeños, and sour cream. 
-$22 The Four Leaf Clover (Mix of the 4 appetizers)
+          <br></br>
+          <br></br>
+      <h2><br></br>Pub's Dazzlin' Classics!</h2>
+      <br></br>
+      <div class="row">
+    <div class="col-4">col-8</div>
+    <div class="col-8">
+      <ul>
+      <li>$14.00 - Chicken Quesadillas
+      Grilled flour tortillas stuffed with seasoned chicken, melted cheese, and served with salsa and sour cream.</li>
+      <li>$12.50 -Personal Pizza
+      A perfectly portioned pizza with your choice of toppings, baked to golden, cheesy perfection.</li>
+      <li>$16.00 - The Liquid Gold Stew
+      Slow-cooked beef with root vegetables, all simmered in a rich Guinness broth for deep, comforting flavor.</li>
+      <li>$14.00 - The Classic Comfort
+      Juicy Irish sausages nestled on a bed of buttery mashed potatoes, drenched in savory onion gravy.</li>
+      <li>$18.00 - The Irish Tradition
+      Tender, salty corned beef served with boiled cabbage, hearty potatoes, and sweet carrots.</li>
+      <li>$15.00 - The Potato Pancake Surprise
+      Traditional Irish potato pancake stuffed with your choice of chicken, crispy bacon, or smoked salmon.</li>
+      <li>$22.00 - The Lamb King
+      Grilled lamb chops full of flavor, paired with garlic mashed potatoes and seasonal vegetables.</li>
+      <li>$18.00 - The Coastal Cream
+      Fresh mussels simmered in a creamy garlic and white wine sauce, served with warm, crusty bread.</li>
+      </ul>
+        </div>
+  </div>
+
       </div>
-
-      {/*  
-
-Chicken Quesadillas …  $14.00
-Grilled flour tortillas stuffed with seasoned chicken, melted cheese, and served with salsa and sour cream.
-
-
-Personal Pizza
- A perfectly portioned pizza with your choice of toppings, baked to golden, cheesy perfection.
- $12.50
-The Liquid Gold Stew
- Slow-cooked beef with root vegetables, all simmered in a rich Guinness broth for deep, comforting flavor.
- $16.00
-The Classic Comfort
- Juicy Irish sausages nestled on a bed of buttery mashed potatoes, drenched in savory onion gravy.
- $14.00
-The Irish Tradition
- Tender, salty corned beef served with boiled cabbage, hearty potatoes, and sweet carrots.
- $18.00
-The Potato Pancake Surprise
- Traditional Irish potato pancake stuffed with your choice of chicken, crispy bacon, or smoked salmon.
- $15.00
-The Lamb King
- Grilled lamb chops full of flavor, paired with garlic mashed potatoes and seasonal vegetables.
- $22.00
-The Coastal Cream
- Fresh mussels simmered in a creamy garlic and white wine sauce, served with warm, crusty bread.
- $18.00
-
-$6	Mozzarella Sticks ~or~ French Fries ~or~ Onion Rings
-$8	Potato Skins – Crispy potato halves loaded with cheese, 	bacon, and sour cream. 
-$10	Chicken Wings (BBQ, Buffalo, Mild, Teriyaki)
-$12	Irish Nachos – Sliced potatoes topped with melted cheese, 	jalapeños, and sour cream. 
-$22 The Four Leaf Clover (Mix of the 4 appetizers) */}
     </div>
   );
 }
