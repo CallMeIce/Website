83f73c5eb9ef9655de9c6a4ffabc535c9d12ccbf
Isaak Morales <ice.isaak97@gmail.com>
2025-05-03 01:37:26 -0400

    added a background and lowered text in staff bios

--- src/components/Meet.js
+++ src/components/Meet.js
@@ -6,6 +6,7 @@ export default function MeetUs() {
   return (
     <div className="container">
     <h1 className="textMeet">Meet the Team</h1>
+    <br></br>
 
 <div>
     <div id="carouselExampleCaptions" class="carousel slide," >
@@ -19,7 +20,15 @@ export default function MeetUs() {
   <div class="carousel-inner,">
     <div class="carousel-item active">
       <img src={require('../images/luckycharms.jpg')}  height={600} class="d-block w-100" alt="Matt"></img>
-      <div class="carousel-caption d-none d-md-block">
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div class="carousel-caption d-none d-md-block text-white bg-success">
         <h5 className="textMeet">Matt</h5>
         <p className="textMeet">With a deep Irish heritage, Matt has a love and passion for Irish culture and serving the community.
             You can find him behind the bar pouring a proper pint of Guinness</p>
@@ -27,8 +36,20 @@ export default function MeetUs() {
     </div>
     <div className="staffPics" class="carousel-item">
       <img  src={require('../images/robert.jpg')} height={600} class="d-block w-100" alt="Abdul"></img>
-      <div class="carousel-caption d-none d-md-block">
-        <h5 className="textMeet" style={{ color: 'black' }}>Abdul</h5>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div class="carousel-caption d-none d-md-block text-white bg-success">
+        <h5 className="textMeet">Abdul</h5>
         <p className="textMeet">Abdul Rashedy takes the helm of Pub, blending bold expertise with warm hospitality. 
           With his signature tough love, he welcomes guests to a lively space filled with rich stews, perfectly
            poured pints, and hearty laughter, ensuring every dish and experience captures the soul of Ireland</p>
@@ -36,7 +57,17 @@ export default function MeetUs() {
     </div>
     <div class="carousel-item">
     <img src={require('../images/waluigi.jpg')} height={600} class="d-block w-100" alt="Sebastion"></img>
-      <div class="carousel-caption d-none d-md-block">
+    <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div class="carousel-caption d-none d-md-block text-white bg-success">
         <h5>Sebastion</h5>
         <p>with years spent perfecting my craft, traveling across the world and acquainting myself with 
           the art of a fine drink. The Pub allows for my expertise to shine in an environment filled
@@ -46,7 +77,15 @@ export default function MeetUs() {
     </div>
     <div class="carousel-item">
     <img src={require('../images/frank.jpg')} height={600} class="d-block w-100" alt="Ryan"></img>
-      <div class="carousel-caption d-none d-md-block">
+    <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div class="carousel-caption d-none d-md-block text-white bg-success">
         <h5>Ryan</h5>
         <h6>Serving with a smile and recommending his favorite drinks and dishes, Ryan
           helps to keep Pub a pleasant experience.
@@ -55,7 +94,14 @@ export default function MeetUs() {
     </div>
     <div class="carousel-item">
     <img src={require('../images/spongebob.jpg')} height={600} class="d-block w-100" alt="Ryan"></img>
-      <div class="carousel-caption d-none d-md-block">
+    <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div class="carousel-caption d-none d-md-block text-white bg-success">
         <h5>Isaak</h5>
         <h6>With years of flipping patties, it was time for a change. Working at Pub exposed me to make new creations
           rather than just making a Krabby Patty everyday.
