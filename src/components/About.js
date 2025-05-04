bc1569ca576e676e0a6c06a9a03fba8aa5f3ed9f
Isaak Morales <ice.isaak97@gmail.com>
2025-05-03 22:44:41 -0400

    added gif to about us

--- src/components/About.js
+++ src/components/About.js
@@ -14,14 +14,30 @@ export default function About() {
     
     // }
 
-return (<div className="container">
+return (
+<div className="container">
     <div>
         <div className="aboutMsg"> 
             <p className="textHours" class="list-group-item list-group-item-success rounded"> Located near the East Village.
             Pub is a well known pub where you can relax, unwind, and enjoy making some
             new memories! </p>
-            </div>
+            </div>  
+             <br></br>    
+             <br></br>    
+             <br></br>    
+             <br></br>    
      </div>
+     <br></br>
+        <br></br>
+     <div >
+        <span>
+     <img  src={require('../images/cheers.gif')} class="min-vw-50" alt="Cheers"></img>
+</span>
+            </div>
+            <br></br>
+            <div>
+
+            </div>
 </div>
 
 
