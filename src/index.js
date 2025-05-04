98a77970400f1bfed619e2c274ab8a7c6c12db41
Isaak Morales <ice.isaak97@gmail.com>
2025-05-04 00:04:24 -0400

    trying hashroutes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './App.css';
 import './components/style.css'
 import Header from './components/Header';
@@ -9,10 +9,10 @@ import Header from './components/Header';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <Header />
     <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
